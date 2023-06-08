c84a5c9a2eb54e91f584b98a91fd088538520fd1
Firebird492 <gavinbuier@outlook.com>
2023-06-08 08:46:02 -0400

    Got the remove op working along with testing it

--- operations.py
+++ operations.py
@@ -1,7 +1,25 @@
 from pymongo import MongoClient
 
+#connection string: mongodb+srv://gavinbuier:<password>@userinformation.g0x0e9q.mongodb.net/
+#Database = GroupWare
+#Collection = user_information
 
 
+#finds all people with the given name
+def findPerson(name):
+        # Replace the uri string with your MongoDB deployment's connection string.
+    uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority"
+    client = MongoClient(uri)
+    # database and collection code goes here
+    db = client.sample_guides
+    coll = db.planets
+    # find code goes here
+    cursor = coll.find({"Name": name})
+    # iterate code goes here
+    for doc in cursor:
+        print(doc)
+    # Close the connection to MongoDB when you're done.
+    client.close()
 
 
 
@@ -10,17 +28,17 @@ from pymongo import MongoClient
 #adds a person to the database
 def addPerson(name):
     # Replace the uri string with your MongoDB deployment's connection string.
-    uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority"
+    #uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority"
+    uri = "mongodb+srv://gavinbuier:IeljglDxt5Gew8U1@userinformation.g0x0e9q.mongodb.net/?retryWrites=true&w=majority"
     client = MongoClient(uri)
     # database and collection code goes here
-    db = client.sample_guides
-    coll = db.comets
+    db = client.GroupWare
+    coll = db.user_information
     coll.drop()
     # insert code goes here
     docs = [
-	    {"name": "Halley's Comet", "officialName": "1P/Halley", "orbitalPeriod": 75, "radius": 3.4175, "mass": 2.2e14},
-	    {"name": "Wild2", "officialName": "81P/Wild", "orbitalPeriod": 6.41, "radius": 1.5534, "mass": 2.3e13},
-	    {"name": "Comet Hyakutake", "officialName": "C/1996 B2", "orbitalPeriod": 17000, "radius": 0.77671, "mass": 8.8e12},
+	    {"Name": name,},
+	    
     ]
     result = coll.insert_many(docs)
     # display the results of your operation
@@ -35,20 +53,27 @@ def addPerson(name):
 #removes a person from the database
 def removePerson(name):
     # Replace the uri string with your MongoDB deployment's connection string.
-    uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority"
+    uri = uri = "mongodb+srv://gavinbuier:IeljglDxt5Gew8U1@userinformation.g0x0e9q.mongodb.net/?retryWrites=true&w=majority"
     client = MongoClient(uri)
-    db = 1      #client.sample_guides (sample guides is the name of the db)
-    coll = 1     #db.comets (comets is the name of the collection)
+    db = client.GroupWare      #client.sample_guides (sample guides is the name of the db)
+    coll = db.user_information     #db.comets (comets is the name of the collection)
     # delete code goes here
     doc = {
-    "Name": {
+    "name": {
         "$eq": name,
         }
     }
-    result = coll.delete_many(doc)
     result = coll.delete_many(doc)
     # amount deleted code goes here
     print("Number of documents deleted: ", result.deleted_count)
     # Close the connection to MongoDB when you're done.
     client.close()
-    return
+    return result.deleted_count
+
+
+
+def main():
+    uri = "mongodb+srv://gavinbuier:IeljglDxt5Gew8U1@userinformation.g0x0e9q.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(uri)
+    # database and collection code goes here
+    db = client.sample_guides
--- testing.py
+++ testing.py
@@ -1,10 +1,25 @@
+import operations
 
 
 
 
 
 
-
-
+#Testing the operations in operations.py
 def testOperations():
+    name = "Gavin Buier"
+    print("begin remove")
+    removed = operations.removePerson(name)
+    assert(removed>=1)
+    print("ending remove")
     return
+
+
+def startTesting():
+    print("Starting Tests")
+    testOperations()
+    print("Tests over")
+
+
+
+startTesting()
