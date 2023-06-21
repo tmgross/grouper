0ec70e0adfc5eb2adad1e07453ce203f55ee8e05
ragar3 <qN%ZMi6Q3nq7Qi>
2023-06-21 13:16:17 -0400

    worked on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import AddButton from './components/AddButton.tsx'
 import RemoveButton from './components/RemoveButton.tsx'
 import Header from './components/Header.tsx'
 import InputBox from './components/InputBox.tsx'
 
 
+
 function App(){
-  return (<div><Header  />
-  <InputBox/>
-  <AddButton/>
-  <RemoveButton/>
+
+  // const [userList, setUserList] = useState([{}]);
+  const [name, setName] = useState('');
+
+/*
+  // get all users
+  useEffect( () => {
+    axios.get('https://localhost:8000/api/user')
+    .then(res => {
+      setUserList(res.data)
+    })
+  });*/
+
+  // add a user
+  const addUserHandler = () => {
+    axios.post('https://localhost:8000/api/user/', {'name': name})
+    .then(res => console.log(res))
+    .catch(error => console.log(error))
+  };
+
+
+
+return (
+  <div>
+    <Header/>
+    <input 
+      className="form-control nameIn" 
+      type="text" 
+      placeholder="Enter Name" 
+      onChange={event => setName(event.target.value)}/> 
+    <button 
+      type="button" 
+      onClick={addUserHandler}
+    >
+      Join Group
+    </button>
+    <RemoveButton/>
   </div>
-  
   )
 }
 
 export default App;
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
