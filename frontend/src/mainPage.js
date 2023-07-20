0deece92c222ac02fccfb102ada119b4fe0135d7
tmgross <tmgross8@gmail.com>
2023-07-20 17:01:55 -0400

    Added friends box to main page

--- frontend/src/mainPage.js
+++ frontend/src/mainPage.js
@@ -4,22 +4,39 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function location() {
+function Location() {
 
   return (
     <div className="centered">
       <h1 className="location company-name">grouper</h1>
-      <div className="textbox-container">
-        <h2>Your Groups</h2>
-        <div class="button-container">
-            <button>Button 1</button>
-            <button>Button 2</button>
-            <button>Button 3</button>
-            <button>Button 4</button>
+        <div style={{ display: 'flex' }}>
+            <div className="textbox-container">
+                <h2>Your Friends</h2>
+                <textarea
+                    id="friendsTextBox"
+                    rows="11"
+                    cols="30"
+                    readonly
+                    disabled
+                    className='friends-list'
+                    style={{  resize: 'none' }}
+                >
+                This is a large text box that users can't edit.
+                </textarea>
+            </div>
+         
+            <div className="textbox-container">
+                <h2>Your Groups</h2>
+                <div class="button-container">
+                    <button>Button 1</button>
+                    <button>Button 2</button>
+                    <button>Button 3</button>
+                    <button>Button 4</button>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 }
 
-export default location;
+export default Location;
