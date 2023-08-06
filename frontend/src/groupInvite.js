81e1332dbbcb1e7f6a737a2750f635269e28f6fc
tmgross <tmgross8@gmail.com>
2023-08-06 14:49:27 -0400

    Updating component locations

--- frontend/src/groupInvite.js
+++ frontend/src/groupInvite.js
@@ -10,12 +10,14 @@ import './center.css';
 function GroupInvitePage() {
     const [friend, setFriend] = useState('');
 
-    const handleFriend = async () => {
-      axios.get(`http://localhost:8000/api/user/${friend}`)
-        .then(res => {
-          console.log(res.data);
-        })
-        .catch(e => console.log(e))
+    const handleInvite = async () => {
+    //Replace this with proper function for inviting to group
+
+    //   axios.get(`http://localhost:8000/api/user/${friend}`)
+    //     .then(res => {
+    //       console.log(res.data);
+    //     })
+    //     .catch(e => console.log(e))
     };
 
   return (
@@ -26,35 +28,48 @@ function GroupInvitePage() {
       <h1 className="location">Invite to Group</h1>
       <h4>Select a friend or enter a friend code</h4>
 
-      <div className="textbox-container">
-        <input
-          className="form-control nameIn"
-          type="text"
-          placeholder="Enter Friend Code"
-          onChange={(e) => setFriend(e.target.value)}
-        />
-
+      <div style={{ display: 'flex' }}>
         <div className="textbox-container">
-                <text>
-                    This code belongs to:
-                </text>
+                <h2>Friends</h2>
                 <textarea
-                  id="friendsNameTextBox"
-                  readonly
-                  disabled
-                  className='friends-name'
-                  style={{  resize: 'none' }}
+                    id="friendsTextBox"
+                    rows="11"
+                    cols="30"
+                    readonly
+                    disabled
+                    className='friends-list'
+                    style={{  resize: 'none' }}
                 >
-                  Friend Name
+                  This is a large text box that users can't edit.
                 </textarea>
-            </div>
-          <Button variant="contained" type="button" style={{width: '200px'}} 
-          onClick={handleFriend}>
-            Send Invite
-          </Button>
         </div>
-
-
+        <div className="textbox-container" style={{marginTop: '130px'}}>
+          <input
+            className="form-control nameIn"
+            type="text"
+            placeholder="Enter Friend Code"
+            onChange={(e) => setFriend(e.target.value)}
+          />
+          <div className="textbox-container">
+            <text>
+              This code belongs to:
+            </text>
+              <textarea
+                id="friendsNameTextBox"
+                readonly
+                disabled
+                className='friends-name'
+                style={{  resize: 'none' }}
+              >
+                Friend Name
+              </textarea>
+          </div>
+            <Button variant="contained" type="button" style={{width: '200px'}} 
+            onClick={handleInvite}>
+              Send Invite
+            </Button>
+        </div>
+      </div>
     </div>
   );
 }
